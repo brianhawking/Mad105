efdbc53492de72f8f8cf466709c8ea913e68d36a
Brian Veitch <beveitch@gmail.com>
2021-09-03 10:21:03 -0500

    initial commit

---
+++ Mad105-AboutMe/src/main.kt
@@ -0,0 +1,7 @@
+fun main() {
+    println("My name is Brian Veitch")
+    println("I'm working towards an associates in mobile app design and development.")
+    println("I work as a math instructor at Northern Illinois and adjunct at Mchenry County College")
+    println("I'm married with 2 kids.")
+    println("I love playing board games.")
+}
