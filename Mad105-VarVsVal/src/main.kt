f332a396804909f91d23058c27de40738b5eb501
Brian Veitch <beveitch@gmail.com>
2021-09-03 12:37:30 -0500

    initial commit

---
+++ Mad105-VarVsVal/src/main.kt
@@ -0,0 +1,20 @@
+fun main() {
+    // data types
+    val myByte: Byte = 3
+    val myShort: Short = -10
+    val myInt: Int = 34234301
+    val myLong: Long = -34309823409832
+    val myFloat: Float = 3.14159F
+    val myDouble: Double = -2.343214333
+    val myBoolean: Boolean = false
+    val myChar: Char = 'v'
+
+    println("byte demo: $myByte")
+    println("short demo: $myShort")
+    println("int demo: $myInt")
+    println("long demo: $myLong")
+    println("float demo: $myFloat")
+    println("double demo: $myDouble")
+    println("boolean demo: $myBoolean")
+    println("char demo: $myChar")
+}
