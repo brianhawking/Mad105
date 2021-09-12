4a6cfd334f7c4f911bee7f4689e1df74ba5517c9
Brian Veitch <beveitch@gmail.com>
2021-09-12 12:57:46 -0500

    update variable declarations

--- Mad105-VarVsVal/src/main.kt
+++ Mad105-VarVsVal/src/main.kt
@@ -2,12 +2,14 @@ fun main() {
     // data types
     val myByte: Byte = 3
     val myShort: Short = -10
-    val myInt: Int = 34234301
+//    val myInt: Int = 34234301 - wants me to remove the type declaration
+    val myInt = 34234301
     val myLong: Long = -34309823409832
-    val myFloat: Float = 3.14159F
+//    val myFloat: Float = 3.14159F - wants me to remove type declaration
+    val myFloat =  3.14159F
     val myDouble: Double = -2.343214333
-    val myBoolean: Boolean = false
-    val myChar: Char = 'v'
+    val myBoolean = false
+    val myChar = 'v'
 
     println("byte demo: $myByte")
     println("short demo: $myShort")
