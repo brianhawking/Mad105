03880f5d8b593ed4a10448771a2cc3aa05b97732
Brian Veitch <beveitch@gmail.com>
2021-09-20 16:00:09 -0500

    initial commit

--- Mad105-Arrays]/src/main.kt
+++ Mad105-Arrays]/src/main.kt
@@ -1,27 +1,26 @@
-/*
-Create an array that lists type of supplies for a simple hot dog stand, ice cream shop, coffee shop or something similar.
-Use a for loop to print the contents of the array on the screen. You will use this as the start of the next program.
-You need at least 5 items in your array.
- */
+/********************************************************************
+Mad 105 - Intro to Arrays
+
+Programmer: Brian Veitch
+
+Purpose:   Define and use an array. Print elements of the array to screen.
+ Loop through array two different ways.
+ *********************************************************************/
+
 
 fun main() {
-    val hotDogItems = arrayOf("hot dog", "bun", "ketchup", "mustard", "salt", "pickle", "tomatoes")
-    val iceCreamItems = arrayOf("vanilla", "chocolate", "strawberry", "cone", "bowl", "chocolate chips", "sprinkles")
-    val smoothieItems = arrayOf("blender", "milk", "strawberries", "banana", "apple", "acai", "blueberries", "peach", "small", "medium", "large")
+    val hotDogItems = arrayOf("hot dog", "bun", "ketchup", "mustard", "salt", "pickle", "tomatoes", "relish", "onion")
 
     println("Hot dog stand items")
     for (item in hotDogItems) {
         println(item)
     }
 
-    println("Ice cream shop items")
-    for (i in 0 until iceCreamItems.size -1) {
-        println(iceCreamItems[i])
-    }
+    println("\n======================================================\n")
 
-    println("Smoothie ship items")
-    for (item in smoothieItems) {
-        println(item)
+    println("Hot dog stand items")
+    for (i in 0 until hotDogItems.size -1) {
+        println(hotDogItems[i])
     }
 
 }
